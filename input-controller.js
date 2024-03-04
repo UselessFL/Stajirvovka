ba87157ec0625c486f5eb0932572517825775d06
Denis <Denis1231123@mail.ru>
2024-03-04 14:05:14 +0300

    a

--- input-controller.js
+++ input-controller.js
@@ -3,38 +3,24 @@ class InputController{
    
     constructor(target,actionsToBind={}){
         this.target = target;
+        this.actionsToBind=actionsToBind;
         this.actions={};
         this.enabled = true;
         this.focused = true;
          this.ACTION_ACTIVATED = 'input-controller:action-activated'
         this.ACTION_DEACTIVATED = 'input-controller:action-deactivated'
-        this.bindActions (actionsToBind);
+        /* this.bindActions (actionsToBind); */
        
     }
-/*     initKeyboard(){
-    
-        
-        document.addEventListener('keydown', (event)=>{
-            this.handleInput(event.key)
-        })
-    }
-    handleInput(key){
-        if(this.keyMap[key]){
-            this.keyMap[key]()
-        }
-    }
-    mapKey(key,action){
-        this.keyMap[key] = action;
-    }
-    unmapKey(key){
-        delete this.keyMap[key]
-    } */
 
-    /* bindActions(actionsToBind){
-        this.enableAction(actionsToBind.actions)
+
+    bindActions(actionsToBind){
+        this.enableAction(actionsToBind.actions[actionName])
     }
     enableAction(actionName){
-        this.isActionActive? actionName.:' '
+        if(this.isActionActive(actionName)){
+            
+        }
     }
     disableAction(actionName){
 
@@ -46,60 +32,30 @@ class InputController{
         
     }
     isActionActive(action){
-
-        return
+        if (action){
+            return true
+        }
+        else{return false}
     }
     isKeyPressed(keyCode){
-        return
-    } */
-    bindActions(actionsToBind){
-        for(const actionName in actionsToBind){
-            const action = actionsToBind[actionName];
+        if(key == keyCode){
+            return true
         }
-        this.actions[actionName] = {
-            keys:[...action.keys]
-        }
-    }
-    enableAction(actionName){
-        if(this.actions[actionName]){
-            this.actions[actionName].enabled = true;
-        
-        }
-    }
-    disableAction(actionName){
-
-    }
-    attach(target){
-        target.addEventListener('keydown', this.bindActions(this.actionsToBind))
-    }
-    detach(){
-        
-    }
-    isActionActive(actionName){
-        const action = this.actions[actionName];
-        if(!action.enabled || this.focused){
+        else{
             return false
         }
-        for(const key of actions.keys){
-            if(this.keyState[key]){
-                return true
-            }
-        }
-        
     }
-    isKeyPressed(keyCode){
-        return
-    }
+    
 }
 const moves ={
-    actions:{
+    
     'right':{
         keys:[39,68]
     },
     'left':{
         keys:[37,65]
     }
-    }
+    
 }
 moves.actions
 let keyboard = new InputController('body');
